7f02065c66d75fb41919453df90431968d1e3012
UPCSM SKILL <walasabkuch@gmail.com>
2021-07-04 22:48:27 +0530

    Project java

---
+++ src/Hifun.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Hifun {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello , this is cool program"
+        );
+        System.out.println("hello , this is my first java class"
+        );
+
+    }
+
+
+}
